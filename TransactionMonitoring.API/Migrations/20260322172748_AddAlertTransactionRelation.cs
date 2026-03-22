55f770b300706f92b1c7a0d06d67e5fd8359a9b9
ty <tyronnyambe@gmail.com>
2026-03-22 19:32:09 +0200

    Altered Alert.cs adding Transaction field to create relationship between an alert and a transaction

---
+++ TransactionMonitoring.API/Migrations/20260322172748_AddAlertTransactionRelation.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransactionMonitoring.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAlertTransactionRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Alerts_TransactionId",
+                table: "Alerts",
+                column: "TransactionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Alerts_Transactions_TransactionId",
+                table: "Alerts",
+                column: "TransactionId",
+                principalTable: "Transactions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Alerts_Transactions_TransactionId",
+                table: "Alerts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Alerts_TransactionId",
+                table: "Alerts");
+        }
+    }
+}
--- TransactionMonitoring.API/Models/Alert.cs
+++ TransactionMonitoring.API/Models/Alert.cs
@@ -3,6 +3,7 @@ namespace TransactionMonitoring.API.Models;
 public class Alert
 {
     public int Id { get; set; } //Primary Key
+    public Transaction Transaction { get; set; } = null!;
     public int TransactionId { get; set; } //Link to Transaction
     public string RuleName { get; set; } = string.Empty; //Which Rule triggered
     public DateTime CreatedAt { get; set; } //When was the alert created
