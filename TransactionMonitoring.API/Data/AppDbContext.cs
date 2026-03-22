b7ee55be8fd354741ed3facc440259c5e5144258
ty <tyronnyambe@gmail.com>
2026-03-22 14:49:31 +0200

    Added AppDbContext to manage database entities

--- TransactionMonitoring.API/Data/AppDbContext.cs
+++ TransactionMonitoring.API/Data/AppDbContext.cs
@@ -3,12 +3,14 @@ using TransactionMonitoring.API.Models;
 
 namespace TransactionMonitoring.API.Data;
 
-//The bridge between your code and the database
+//The bridge between code and the database
 public class AppDbContext : DbContext
 {
+    //Contructor used by Dependency Injection
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) { }
 
-    public DbSet<Transaction> Transactions { get; set; }
-    public DbSet<Alert> Alerts { get; set; }
+    //These represent tables in the database.
+    public DbSet<Transaction> Transactions { get; set; } //Create table Transactions based on the Transactions model.
+    public DbSet<Alert> Alerts { get; set; } //Create table Alerts based on the Alerts model
 }
