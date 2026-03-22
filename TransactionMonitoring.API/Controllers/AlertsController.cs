b75c4870ccc78e077454413443bbaf2bab5db3cd
ty <tyronnyambe@gmail.com>
2026-03-22 19:32:31 +0200

    Added AlertsController with filtering by account and rule; linked Alert to Transaction

---
+++ TransactionMonitoring.API/Controllers/AlertsController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TransactionMonitoring.API.Data;
+
+namespace TransactionMonitoring.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AlertsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public AlertsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // Get all alerts
+    [HttpGet]
+    public async Task<IActionResult> GetAllAlerts()
+    {
+        var alerts = await _context.Alerts.ToListAsync();
+        return Ok(alerts);
+    }
+
+    // Get alerts by account
+    [HttpGet("by-account/{accountId}")]
+    public async Task<IActionResult> GetAlertsByAccount(string accountId)
+    {
+        var alerts = await _context.Alerts
+            .Include(a => a.Transaction)
+            .Where(a => a.Transaction.AccountId == accountId)
+            .ToListAsync();
+
+        return Ok(alerts);
+    }
+
+    // Get alerts by rule
+    [HttpGet("by-rule/{ruleName}")]
+    public async Task<IActionResult> GetAlertsByRule(string ruleName)
+    {
+        var alerts = await _context.Alerts
+            .Where(a => a.RuleName == ruleName)
+            .ToListAsync();
+
+        return Ok(alerts);
+    }
+}
